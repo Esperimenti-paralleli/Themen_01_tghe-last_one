89080fedd070b922096bbca6b6ce4523f9227075
7Math <168723211+7Math@users.noreply.github.com>
2024-05-13 08:11:01 +0000

    Fine controlflow inizio funzioni

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -4,19 +4,19 @@
 const ageJohn = 30;
 const ageMark = 20;
 
-// Deklaration
-let isJohnOlder, isJohnEqual;
+// // Deklaration
+// let isJohnOlder, isJohnEqual;
 
-// Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+// // Test | Logische Aussage
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
-// Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// // Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -47,7 +47,7 @@ console.log("------------------");
 // }
 
 /************  Ternäre (ternary) Schreibweise ************/
-console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
 
 
 /************ IF - ELSE IF  ************/
@@ -66,5 +66,38 @@ console.log( (isJohnOlder) ? "John is older" : "Mark is older");
 //     console.log("John ist jünger!");
 // }
 
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
 
+const firstName = "Jane";
+let job;
+
+job = "driver";  // .. fährt TAXI! / UBER
+job = "diver"; // .. taucht im Rhein!
+job = "artist"; // .. malt ein Bild!
+job = "pilot"; // .. macht etwas anderes! --> default
+job = "teacher"; // .. unterrichtet!
+job = "instructor"; // .. unterrichtet!
+
+switch (job) 
+{
+    case "driver": // Test auf Gleichheit ...
+        console.log(firstName + "..fährt TAXI!");
+        break;
+    case "diver": // Test auf Gleichheit ...
+    console.log(firstName + ".. malt ein Bild!");
+    break;
+    case "artist": // Test auf Gleichheit ...
+    console.log(firstName + ".. taucht im Rhein!");
+    break;
+    case "teacher": // Test auf Gleichheit ...
+    case "instructor": // Test auf Gleichheit ...
+    console.log(firstName + ".. unterrichtet!");
+    break;
+
+
+default:
+    console.log(firstName + ".. macht etwas anderes!");
+
+        break;
+}
 
