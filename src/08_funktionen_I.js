46c1f194afaa7c1fb7513d9806d8e3135627591f
7Math <168723211+7Math@users.noreply.github.com>
2024-05-13 08:35:29 +0000

    Funktion 1

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -1,4 +1,25 @@
 /***** Funktionen 01 *****/
-// 1. Kapselung von CodeblÃ¶cken
+// 1. Kapselung von Codeblöcken
+// console.log("Hallo, Esther!");
 
+// Funktionsrumpf call(er)
+// test();
+
+// Funktionsrumpf | callee
+// Funktionsdeklaration
+function test() 
+{
+    console.log("Hallo, Esther!"); 
+}
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// Funktionsrumpf
+outputNames();
+
+// Funktionsdeklaration
+function outputNames() {
+    console.log("Hallo, Esther!"); 
+}
 
